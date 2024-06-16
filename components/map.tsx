71e6d4ef292affb24d5a98be9199905923198737
williamtuominiemi <william.m.tuominiemi@gmail.com>
2024-06-16 17:22:44 +0300

    draw circle around placed marker

--- components/map.tsx
+++ components/map.tsx
@@ -1,28 +1,58 @@
 'use client';
 
-import { GoogleMap, Marker } from '@react-google-maps/api';
-import { useState } from 'react';
+import { GoogleMap, Marker, Circle } from '@react-google-maps/api';
+import React, { useState, useRef, useCallback } from 'react';
 
 export const defaultMapContainerStyle = {
   width: '100%',
   height: '100vh',
 };
 
+const range = 50000;
+
 const defaultMapCenter = new google.maps.LatLng(60.192059, 24.945831);
 
 const defaultMapZoom = 7;
 
 const defaultMapOptions = {
-  mapId: '1c93ead7672f925d',
+  mapId: process.env.MAP_ID,
   disableDefaultUI: true,
   zoomControl: true,
 };
 
 const MapComponent = () => {
-  const [markerCoord, setMarkerCoord] = useState<google.maps.LatLng>(defaultMapCenter);
+  const [markerCoord, setMarkerCoord] = useState<google.maps.LatLng | null>(null);
+  const [circle, setCircle] = useState<google.maps.Circle | null>(null);
+  const mapRef = useRef<google.maps.Map | null>(null);
 
   const onMapClick = (e: google.maps.MapMouseEvent) => {
-    if (e.latLng) setMarkerCoord(new google.maps.LatLng(e.latLng.lat(), e.latLng.lng()));
+    if (e.latLng) {
+      const coordinate = new google.maps.LatLng(e.latLng.lat(), e.latLng.lng());
+
+      setMarkerCoord(coordinate);
+
+      if (circle) {
+        circle.setMap(null);
+      }
+
+      const newCircle = new window.google.maps.Circle({
+        strokeColor: '#949494',
+        strokeOpacity: 0.5,
+        strokeWeight: 0.5,
+        fillColor: '#AA98A9',
+        fillOpacity: 0.1,
+        radius: range,
+        center: coordinate,
+        clickable: false,
+        map: mapRef.current,
+      });
+
+      setCircle(newCircle);
+    }
+  };
+
+  const onLoad = (map: google.maps.Map): void => {
+    mapRef.current = map;
   };
 
   return (
@@ -33,14 +63,19 @@ const MapComponent = () => {
         zoom={defaultMapZoom}
         options={defaultMapOptions}
         onClick={onMapClick}
+        onLoad={onLoad}
       >
-        <Marker
-          position={markerCoord}
-          icon={{
-            url: 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-            scaledSize: new window.google.maps.Size(40, 40),
-          }}
-        />
+        {markerCoord && (
+          <>
+            <Marker
+              position={markerCoord}
+              icon={{
+                url: 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+                scaledSize: new window.google.maps.Size(20, 20),
+              }}
+            />
+          </>
+        )}
       </GoogleMap>
     </div>
   );
--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+    MAP_ID: process.env.GOOGLE_MAPS_MAP_ID,
   },
 };
 
